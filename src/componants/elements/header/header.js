c99a4d38e66f7abecfa69a72c519ed8cd9e3abb9
ShyguyMatt <mjherford@gmail.com>
2024-03-11 12:11:15 -0400

    boiler plate

---
+++ src/componants/elements/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './style.scss'
+
+export default function Header() {
+
+  return(
+    <header>
+      <h1>This is the header for my page</h1>
+    </header>
+  )
+}
---
+++ src/componants/elements/navbar/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+
+  return(
+    <nav>
+      <h3>this is the navigation for my page</h3>
+    </nav>
+  )
+}
